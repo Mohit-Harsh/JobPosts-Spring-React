ee1063e1e2d0ac5a67d5f15076cf2dc31bf63e93
P-Mohit-Harsh <111693866+Mohit-Harsh@users.noreply.github.com>
2024-03-22 11:35:59 +0530

    Delete JobPosts-Mongo directory

--- JobPosts-Mongo/src/main/java/com/jobs/JobPostsMongo/repository/JobPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobs.JobPostsMongo.repository;
-
-import com.jobs.JobPostsMongo.model.JobPost;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobPostRepository extends MongoRepository<JobPost, Integer>{
-}
